233af20eeb7c5f939316b22727da9e38a2ef981d
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2020-11-04 23:07:13 -0300

    Final clase 10. Manejo de estaticos

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const productRouter = require("./routes/product");
 const app = express();
 
+app.use("/statics", express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
